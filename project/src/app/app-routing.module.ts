fe9e4052a3638bf779c8e6d5408188125f9eb095
Skafii <piotrtkaczyk21a@wp.pl>
2023-02-22 14:53:00 +0100

    projekty

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   {
+    path : '',
+    component: AboutComponent,
+  },
+  {
     path : 'About',
     component: AboutComponent,
+  },
+  {
+    path : 'Projects',
+    component: ProjectsComponent,
   }
 ];
 
