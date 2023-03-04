9535a90d1e042999c894538ad55c5852850adc42
Piotr Tkaczyk <87760192+TkaczykPiotr@users.noreply.github.com>
2023-03-04 18:21:32 +0100

    strona about scroll i sekcje

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Scroll } from '@angular/router';
 
 
 @Component({
@@ -6,7 +7,59 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+ 
+  tmp : number = 0;
+  previousPageYOffset = 0;
+  private scrolledDown = false;
 
+  constructor(private elementRef: ElementRef) {}
+  
+  
+  // @HostListener('window:scroll', ['$event'])
+  // onScroll(event: Event) {
+  //   const pageYOffset = window.pageYOffset;
+  //   if (pageYOffset > this.previousPageYOffset && !this.scrolledDown) {
+  //     this.scrolledDown = true;
+  //     this.scrollToNextSection();
+  //   } else if (pageYOffset < this.previousPageYOffset) {
+      
+  //   }
+  //   this.previousPageYOffset = pageYOffset;
+
+
+  // }
+
+ 
+
+  async ngOnInit(){
+  
+    const firstSection = document.querySelector('.scrollme')!;
+    firstSection.classList.add('active');
+    
+  }
+
+
+  scrollToNextSection(): void {
+    const currentSection = document.querySelector('.scrollme.active')!;
+    const nextSection = document.querySelectorAll('.scrollme:not(.active)')[this.tmp];
+
+    console.log(currentSection);
+    console.log(nextSection);
+    console.log(this.tmp);
+    if (nextSection) {
+      currentSection.classList.remove('active');
+      nextSection.classList.add('active');
+      nextSection.scrollIntoView({ behavior: 'smooth' });
+      this.tmp += 1;
+    }else {
+      currentSection.classList.remove('active');
+      const firstSection = document.querySelectorAll('.scrollme:not(.active)')[0];
+      firstSection.classList.add('active');
+      this.tmp = 0;
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }
+  
 
 }
